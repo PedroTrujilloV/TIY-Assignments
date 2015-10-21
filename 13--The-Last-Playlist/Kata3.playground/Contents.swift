aaeea0663bff04b0d3007fcd2bf82eccaabc9951
PedroTrujilloV <pedrot211@gmail.com>
2015-10-21 15:39:26 -0400

    Kata 3 done!

--- 13--The-Last-Playlist/Kata3.playground/Contents.swift
+++ 13--The-Last-Playlist/Kata3.playground/Contents.swift
@@ -2,15 +2,52 @@
 //: ## Seconds
 //: Determine the number of seconds in a year and store the number in a variable named ```secondsInAYear```.
 
-var 
+func isLeapYear(year:Int)-> Bool
+{
+    return (year % 4) == 0 && (year/100 % 4) == 0  //condition 1 : is leap because is % 4 and condition 2: is leap because each 100 year if not % 400
+}
+
+func getDaysByKindOfYear(year: Int) -> Int
+{
+    if !isLeapYear(year)
+    {
+        return 365
+    }
+    else
+    {
+        return 366
+    }
+}
+
+
+var year = 2015 // remember set the year
+
+var secondsInMinute = 60
+var minutesInHour = 60
+var hourInDay = 24
+var daysInYear = getDaysByKindOfYear(year)
+
+//var secondsInAYear = secondsInMinute * minutesInHour * hourInDay * daysInYear
+
+//secondsInAYear
 
 
 //: # Question 2
 //: ## Coin Toss
 //: If you use ```arc4random()``` it will give you a random number. Generate a random number and use it to simulate a coin toss. Print "heads" or "tails".
 import Foundation
-var randomNumber = arc4random()
 
+func coinToss() -> String
+{
+    if arc4random() % 2 == 0
+    {return "heads"}
+    else
+    { return "tails"}
+}
+
+//var turn = coinToss()
+
+//turn
 
 
 //: # Question 3
@@ -19,4 +56,10 @@ var randomNumber = arc4random()
 var number = arc4random() % 1000
 
 
+func isDivisibleBy_3_5or7_this(number:Int) -> Bool
+{
+    return number % 3 == 0 && number % 5 == 0 && number % 7 == 0
+}
+
+var ans:Bool = isDivisibleBy_3_5or7_this(Int(number)) //?
 
