355b096a5173a9348704ee48abb2ad3b27a7db32
PedroTrujilloV <pedrot211@gmail.com>
2015-10-26 09:31:56 -0400

    creation assignment 16

---
+++ 14--A-New-HIG/cosa.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var putoSwift = "ijoputaSwift"
+
+var str:String = "Hello, playground"
+
+var cosa = Array(str.characters)//  as Array
+
+var sss = cosa.removeAtIndex(0)
+
+
+putoSwift += "\(sss)"
+
+print(putoSwift)
+
+
+
+
