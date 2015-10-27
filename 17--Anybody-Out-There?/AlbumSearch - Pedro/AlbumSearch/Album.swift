da6db8466aad7874e14b1a375779a19070a4791d
PedroTrujilloV <pedrot211@gmail.com>
2015-10-27 13:00:16 -0400

    creation folder assignment 17

---
+++ 17--Anybody-Out-There?/AlbumSearch - Pedro/AlbumSearch/Album.swift
@@ -0,0 +1,73 @@
+//
+//  Album.swift
+//  AlbumSearch
+//
+//  Created by Pedro Trujillo on 10/27/15.
+//  Copyright © 2015 Pedro Trujillo. All rights reserved.
+//
+
+import Foundation
+
+struct Album
+{
+    let title: String
+    let price: String
+    let thumbnailImageURL: String
+    let largeImageURL: String
+    let itemURL: String
+    let artistURL: String
+    
+    init(name:String, price:String, thumbnailImageURL:String, largeImageURL:String, itemURL: String, artistURL: String)
+    {
+        self.title = name
+        self.price = price
+        self.thumbnailImageURL = thumbnailImageURL
+        self.largeImageURL = largeImageURL
+        self.itemURL = itemURL
+        self.artistURL = artistURL
+    }
+    
+    static func albumsWithJSON(results: NSArray) ->[Album]
+    {
+        var albums = [Album]()
+        
+        if results.count > 0
+        {
+            for result in results
+            {
+                var name = result["artistName"] as? String
+                if name == nil
+                {
+                    name == result["collectionName"] as? String
+                }
+                
+                var price = result["formattedPrice"] as? String
+                if price == nil
+                {
+                    if price = result["collectionPrice"] as? String
+                    {
+                        if price == nil
+                        {
+                            let priceFloat = result["collectionPrice"] as? Float
+                            let nf = NSNumberFormatter()
+                            nf.maximumFractionDigits = 2
+                            if priceFloat != nil
+                            {
+                                price = "$\(nf.stringFromNumber(priceFloat!)!)"
+                            }
+                        }
+                    }
+                    let thumbnailURL = result["artworkUrl60"] as? String?? ""
+                    let imageURL = result["artworkUrl100"] as? String?? ""
+                    let artistURL = result["artistViewUrl"] as? String? ""
+                    var itemURL == nil
+                    {
+                        itemURL = result["trackViewUrl"] as? String
+                    }
+                    
+                
+                }
+            }
+        }
+    }
+}
