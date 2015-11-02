419e670e689c305ce7fc69f03c936429d80ebffc
PedroTrujilloV <pedrot211@gmail.com>
2015-11-02 13:13:58 -0500

    Partial Hard Mode, Just need assign the respective day name

---
+++ 19--Keep-On-The-Sunny-Side/Forecaster/Forecaster/NextSevenDaysTableViewController.swift
@@ -0,0 +1,118 @@
+//
+//  NextSevenDaysTableViewController.swift
+//  Forecaster
+//
+//  Created by Pedro Trujillo on 11/2/15.
+//  Copyright © 2015 Pedro Trujillo. All rights reserved.
+//
+
+import UIKit
+
+class NextSevenDaysTableViewController: UITableViewController
+{
+    var cities = Array<CityData>()
+    
+    var delegator:SearchTableViewProtocol!
+    var cityData:CityData!
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        title = "'s weather next days"
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+       self.tableView.registerClass(CityCell.self, forCellReuseIdentifier: "CityCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return cityData.WeatherWeek.count
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return 80.0
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CityCell", forIndexPath: indexPath) as! CityCell
+        
+        cell.setSummaryLabel(cityData.WeatherWeek[indexPath.row].summary)
+        cell.setTemperaturLabel(cityData.WeatherWeek[indexPath.row].temperature)
+        cell.loadImage(cityData.WeatherWeek[indexPath.row].icon)
+        cell.setNameLabel(String(indexPath.row))
+        // Configure the cell...
+        
+        
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
