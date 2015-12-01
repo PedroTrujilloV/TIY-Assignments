dc0126dc1b459ee1c31a58c7bbb9183bc4a72dcc
PedroTrujilloV <pedrot211@gmail.com>
2015-12-01 00:27:49 -0500

    editing mode (delete ) is done

--- 37--Resurgence/VenueMenu/VenueMenu/VenueMenuTableViewController.swift
+++ 37--Resurgence/VenueMenu/VenueMenu/VenueMenuTableViewController.swift
@@ -52,7 +52,7 @@ class VenueMenuTableViewController: UITableViewController, SearchTableViewProtoc
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
         
         
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
@@ -95,40 +95,35 @@ class VenueMenuTableViewController: UITableViewController, SearchTableViewProtoc
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
+        if editingStyle == .Delete
+        {
+            let aVenue = venuesArray[indexPath.row]
+            venuesArray.removeAtIndex(indexPath.row)
+            managedObjectContext.deleteObject(aVenue)
+            saveContext()
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        }
+//        else if editingStyle == .Insert
+//        {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//        }    
+        tableView.reloadData()
 
     }
-    */
+    
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     
     // MARK: - Navigation
