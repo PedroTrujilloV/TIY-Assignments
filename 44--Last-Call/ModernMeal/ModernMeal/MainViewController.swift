d87e70d464c052e231ff189d3ba0057dda8c8aac
PedroTrujilloV <pedrot211@gmail.com>
2015-12-04 16:20:54 -0500

    add assignment 45 and update MM project

---
+++ 44--Last-Call/ModernMeal/ModernMeal/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModernMeal
+//
+//  Created by Pedro Trujillo on 12/4/15.
+//  Copyright © 2015 Pedro Trujillo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 44--Last-Call/ModernMeal/ModernMeal/RequestController.swift
@@ -0,0 +1,44 @@
+//
+//  RequestController.swift
+//  ModernMeal
+//
+//  Created by Pedro Trujillo on 12/4/15.
+//  Copyright © 2015 Pedro Trujillo. All rights reserved.
+//
+
+import Foundation
+
+class RequestController
+{
+    static func checkUserName(username:String) -> Bool
+    {
+        // code to search in the database for the user:
+        //if this exist return true
+        //else
+        //false
+        return true
+        
+    }
+    
+    static func getUserNameBy(request:String) ->String
+    {
+        var username:String = ""
+        
+        //search user name using the input request  string
+        return username
+    }
+    
+    static func verifySession()
+    {
+    
+    }
+    
+    static func logIng()
+    {
+    }
+    
+    /*
+
+    */
+    
+}
